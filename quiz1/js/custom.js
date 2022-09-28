04f9ceb9473c17bb93b46156894fa35b954c977d
Davian Benito <71868354+davianoh@users.noreply.github.com>
2022-09-28 20:52:51 +0700

    PWeb-update design
    update design css, ngrapiin kodingan

--- quiz1/js/custom.js
+++ quiz1/js/custom.js
@@ -59,13 +59,10 @@ var customScripts = {
             filter: '',
             easing: 'swing',
             begin: function () {
-                //I get fired when the animation is starting
             },
             end: function () {
-                //I get fired when the animation is ending
             },
             scrollChange: function ($currentListItem) {
-                //I get fired when you enter a section and I pass the list item of the section
             }
         });
     },
